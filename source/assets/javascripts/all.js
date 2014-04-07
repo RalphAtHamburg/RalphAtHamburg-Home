9a01f6f4867f099c55aaa4f648c149763fdfa291
Ralph Möcklinghoff <mail@ralph-moecklinghoff.de>
2014-04-07 11:21:12 +0200

    First skeleton with layout classes

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,5 +1,6 @@
 //= require jquery/dist/jquery.min
 //= require foundation/js/foundation.min
+//= require foundation/js/foundation/foundation.topbar.js
 
 //= require_tree ./vendor
 //= require      ./_main
